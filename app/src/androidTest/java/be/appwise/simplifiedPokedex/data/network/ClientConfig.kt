ba2084d281609734fa4d73f3349c5650aa722529
Christiano Bolla <christiano.bolla@appwise.be>
2021-03-28 12:34:54 +0200

    Forgot to delete a couple of things

--- app/src/androidTest/java/be/appwise/simplifiedPokedex/data/network/ClientConfig.kt
+++ app/src/androidTest/java/be/appwise/simplifiedPokedex/data/network/ClientConfig.kt
@@ -5,7 +5,6 @@ import com.google.gson.GsonBuilder
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -27,7 +26,6 @@ object ClientConfig : IClientConfig {
                     }.create()
                 )
             )
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(okHttpConfig().build())
             .build()
     }
--- app/src/release/java/be/appwise/simplifiedPokedex/data/network/ClientConfig.kt
+++ app/src/release/java/be/appwise/simplifiedPokedex/data/network/ClientConfig.kt
@@ -7,7 +7,6 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -37,7 +36,6 @@ object ClientConfig: IClientConfig {
                     }.create()
                 )
             )
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(okHttpConfig().build())
             .build()
     }
