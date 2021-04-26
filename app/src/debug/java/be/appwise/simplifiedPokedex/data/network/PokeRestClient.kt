50655d13aae0482e46141953626c7f85057f3cfd
Christiano Bolla <christiano.bolla@appwise.be>
2021-04-26 11:05:27 +0200

    Added possibility to add a different baseUrl if needed

--- app/src/debug/java/be/appwise/simplifiedPokedex/data/network/PokeRestClient.kt
+++ app/src/debug/java/be/appwise/simplifiedPokedex/data/network/PokeRestClient.kt
@@ -15,14 +15,20 @@ object PokeRestClient: BaseSimpleRestClient() {
     override fun getBaseUrl() = "https://raw.githubusercontent.com/ShaHar91/Simplified-Pokedex/master/json/"
 
     val pokemonService: PokemonNetworkService by lazy {
-        getRetrofit.create(PokemonNetworkService::class.java)
+        // Use this function ('createRetrofit()') when you wish to use a different 'baseUrl' with your service
+        createRetrofit("https://raw.githubusercontent.com/ShaHar91/Simplified-Pokedex/master/json/")
+            .create(PokemonNetworkService::class.java)
     }
 
     val matchUpService: MatchUpNetworkService by lazy {
+        // Use this when you just want to use a different service
         getRetrofit.create(MatchUpNetworkService::class.java)
     }
 
     val baseStatService: BaseStatNetworkService by lazy {
-        getRetrofit.create(BaseStatNetworkService::class.java)
+        // This behaves the same as using 'createRetrofit(baseUrl)',
+        // but offers more options by using the builder itself.
+        getRetrofit.newBuilder().baseUrl("").build()
+            .create(BaseStatNetworkService::class.java)
     }
 }
